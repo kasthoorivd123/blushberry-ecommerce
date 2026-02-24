5f5b7500054f6b3450de281f68ba561769cede32
kasthoorivd123 <kasthoorivd710@gmail.com>
2026-02-24 20:00:58 +0530

    user login and signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,18 @@ const express = require('express')
 const userRouter = express.Router()
 
 const userController = require('../controllers/user/userController')
+const {isLoggedIn,isLoggedOut} = require('../middleware/authMiddleware')
 
 
+userRouter.get('/',isLoggedIn,userController.loadHomePage)
 
-userRouter.get('/',userController.loadHomePage)
-userRouter.get('/signup', userController.loadSignUp)
+userRouter.get('/signup', isLoggedIn,userController.loadSignUp)
+userRouter.get('/login',isLoggedOut,userController.loadLogin) 
 
+userRouter.post('/login',userController.login)
+userRouter.post('/signup',userController.signup) 
+
+userRouter.get('/logout',isLoggedIn,userController.logout)
+// userRouter.post('/verifyOtp',userController.verifyOtp)
 
 module.exports = userRouter
